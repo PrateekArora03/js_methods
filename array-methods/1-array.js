1a19ce78fc8d7b76374c318d27b9c42f6ca8f999
prarora98 <prateekisthebest98@gmail.com>
2019-07-11 15:39:43 +0530

    1-array done

--- array-methods/1-array.js
+++ array-methods/1-array.js
@@ -5,35 +5,71 @@ var strings = ["this", "is", "a", "collection", "of", "words"];
 // Use the above two arrays and practice array methods
 
 // Find largest number in numbers
+Math.max(...numbers);
 
 // Find longest string in strings
+let largestNo = "";
+let length = 0;
+strings.forEach(num => {
+  if (length < num.length) {
+    largestNo = num;
+    length = num.length;
+  }
+});
+console.log(largestNo);
 
 // Find all the even numbers
-//numbers.filter((num)=>num%2==0)
+var evenNO = numbers.filter(num => num % 2 == 0);
+console.log(evenNO);
 
 // Find all the odd numbers
+var oddNO = numbers.filter(num => num % 2 != 0);
+console.log(oddNO);
 
 // Find all the words that contain 'is' use string method 'includes'
+var storeIs = strings.filter(num => num.includes("is"));
+console.log(storeIs);
 
 // Find all the words that contain 'is' use string method 'indexOf'
+// console.log(strings.indexOf('is'));
+let iscontain = [];
+strings.forEach(num => {
+  if (num.indexOf("is") >= 0) iscontain.push(num); //1 is word in the string 0 if find same element -1 word not found in string
+});
 
 // Check if all the numbers in numbers array are divisible by three use array method (every)
+numbers.every(num => num % 3 == 0);
 
 //  Sort Array from smallest to largest
+strings.sort(); // string sort
+
+numbers.sort(function(a, b) {
+  return a - b;
+});
 
 // Remove the last word in strings
 strings.pop();
 // Add a new word in the array
+strings.push("hello");
 
 // Remove the first word in the array
+strings.shift();
 
 // Place a new word at the start of the array use (upshift)
+strings.unshift("px");
 
 // Make a subset of numbers array [18,9,7,11]
+numbers.slice(3, 7);
 
 // Make a subset of strings array ['a','collection']
+strings.slice(2, 4);
 
 // Replace 12 & 18 with 1221 and 1881
+numbers.forEach((num, i) => {
+  if (num == 12) num = num.toString().replace("12", 1221);
+  if (num == 18) num = num.toString().replace("18", 1881);
+  numbers[i] = Number(num);
+});
 
 // Replace words with string in strings array
 
@@ -45,7 +81,15 @@ var customers = [
   { firstname: "Jack", lastname: "White" }
 ];
 // Find all customers whose firstname starts with 'J'
-
+var customersJ = [];
+customers.forEach(obj => {
+  if (obj.firstname.startsWith("J")) customersJ.push(obj.firstname);
+});
 // Create new array with firstname and lastname
+var fullName = [];
+customers.forEach(obj => {
+  fullName.push(obj.firstname + " " + obj.lastname);
+});
 
 // Sort the array created above alphabetically
+fullName.sort();
